2f746f596c6213ca19dbfdaa81e4d7ed21b4afb1
karan-kumardev <karan.ahujakumar@gmail.com>
2026-07-21 21:27:55 +0500

    Stage 2: 404 endpoints read

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 ## Week 2 Task-- CRUD using FastAPI
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app=FastAPI()
 
-@app.get("/hello/{name}")
-def read(name):
-    return {"message":f"Hello,{name} using hello field"}
+storage=[{"id":1, "task":"work", "status":"done"},
+         {"id":2, "task":"watch movie", "status":"pending"},
+         {"id":3, "task":"cook","status":"pending"}
+        ]
+
+@app.get("/tasks/{id}")
+def read(id:int):
+    
+    for target in storage:
+        if target.get("id")==id:
+            return target
+    
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
+
+@app.get("/tasks")
+def get_all():
+
+    return storage
