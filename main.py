622c818950ce9977c9ca1393f0a711468f5e7886
karan-kumardev <karan.ahujakumar@gmail.com>
2026-07-21 23:06:49 +0500

    Stage 3 creating with validation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 ## Week 2 Task-- CRUD using FastAPI
 
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Tasks(BaseModel):
+    title:str
 
 app=FastAPI()
 
@@ -8,6 +12,12 @@ storage=[{"id":1, "task":"work", "status":"done"},
          {"id":2, "task":"watch movie", "status":"pending"},
          {"id":3, "task":"cook","status":"pending"}
         ]
+
+highest_id=3
+
+@app.get("/")
+def root():
+     return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
 
 @app.get("/tasks/{id}")
 def read(id:int):
@@ -23,3 +33,21 @@ def read(id:int):
 def get_all():
 
     return storage
+
+
+@app.post("/tasks", status_code=201)
+def add_tasks(new_tasks:Tasks):
+    global highest_id
+    highest_id+=1
+    next={"id":highest_id,
+            "tasks":new_tasks.title,
+            "status":"pending"
+        }
+
+    if new_tasks.title != "":
+        storage.append(next)
+        return next
+        
+    else:
+        highest_id-=1
+        raise HTTPException(status_code=400, detail="Empty title")    
