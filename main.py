963f4b7fd02b0bb09180dbea8227ee5ca888ca97
karan-kumardev <karan.ahujakumar@gmail.com>
2026-07-27 17:49:14 +0500

    Stage 0: create sqlite db

--- main.py
+++ main.py
@@ -1,81 +1,57 @@
 ## Week 2 Task-- CRUD using FastAPI
-
+import sqlite3
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 class Tasks(BaseModel):
     title:str
 
+conn=sqlite3.connect("to_do.db",check_same_thread=False) # connecting sqlite to this file
 app=FastAPI()
+cursor=conn.cursor() # creating cursor to execute operations
 
-storage=[{"id":1, "task":"work", "status":"done"},
-         {"id":2, "task":"watch movie", "status":"pending"},
-         {"id":3, "task":"cook","status":"pending"}
-        ]
 
-highest_id=3
+cursor.execute("""CREATE TABLE if not exists TASKS( 
+                    
+                  id integer primary key,
+                  title text,
+                  done Bool  
+                  )
+
+            """)
+
+
 
 @app.get("/")
 def root():
      return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
 
-@app.get("/tasks/{id}")
-def read(id:int):
+# @app.get("/tasks/{id}")
+# def read(id:int):
     
-    for target in storage:
-        if target.get("id")==id:
-            return target
+#     for target in storage:
+#         if target.get("id")==id:
+#             return target
     
-    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+#     raise HTTPException(status_code=404, detail=f"Task {id} not found")
 
 
-@app.get("/tasks")
-def get_all():
+# @app.get("/tasks")
+# def get_all():
 
-    return storage
+#     return storage
 
 
 @app.post("/tasks", status_code=201)
-def add_tasks(new_tasks:Tasks):
-    global highest_id
-    highest_id+=1
-    next={"id":highest_id,
-            "tasks":new_tasks.title,
-            "status":"pending"
-        }
+def add_tasks(new_task:Tasks):
 
-    if new_tasks.title != "":
-        storage.append(next)
-        return next
+   if new_task.title != "": 
+     cursor.execute("insert into tasks (title, done) values (?,?)",(new_task.title,False) )
+     conn.commit()
+     id=cursor.lastrowid
+     return {"id":id, "title":new_task.title,"done":False}  
+   else:
+       raise HTTPException(status_code=400,detail="Empty Body ") 
         
-    else:
-        highest_id-=1
-        raise HTTPException(status_code=400, detail="Empty title")    
+        
     
-
-@app.put("/tasks/{id}")
-def update(id:int,task:Tasks):
-
-    for target in storage:
-        if target.get("id")==id:
-          if task.title != '':
-              target["task"]=task.title
-
-              return target
-          else:
-            raise HTTPException(status_code=400,detail="invalid body")
-
-    raise HTTPException(status_code=404, detail="Not found")
-
-
-
-@app.delete("/tasks/{id}",status_code=204)
-def delete(id:int):
-
-    for target in storage:
-
-        if target.get("id")==id:
-            storage.remove(target)
-            return
-    else:    
-        raise HTTPException(status_code=404, detail="not found")        
